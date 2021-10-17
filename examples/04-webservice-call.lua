d802df9ba8a3eba56b157ce234bd2ac55a1ca5ba
Chad Phillips <chad@apartmentlines.com>
2021-10-17 11:07:06 -0400

    add module example

--- examples/04-webservice-call.lua
+++ examples/04-webservice-call.lua
@@ -2,19 +2,40 @@
   Example #4: Calling an external webservice.
 ]]
 
+-- This is a standard way of implementing a consumable module in Lua.
+-- Starting with an empty table.
+local _M = {}
+
 local WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?zip=%s&appid=%s"
 
 local https = require 'ssl.https' -- From LuaSec module.
 local url = require("socket.url") -- From Socket module.
 
-local zip = argv[1] -- The first argument passed to the script.
-local api_key = require("private").openweathermap_api_key -- The require returns a table.
+local default_api_key = require("private").openweathermap_api_key -- The require returns a table.
 
-local endpoint = string.format(WEATHER_URL, url.escape(zip), url.escape(api_key))
+-- Adding a method to the module.
+function _M.get_weather_data(zip, api_key)
+  api_key = api_key or default_api_key
+  local endpoint = string.format(WEATHER_URL, url.escape(zip), url.escape(api_key))
 
-freeswitch.consoleLog("notice", string.format([[Visiting endpoint: %s]], endpoint))
+  freeswitch.consoleLog("notice", string.format([[Visiting endpoint: %s]], endpoint))
 
-local body, status_code, headers, status_description = https.request(endpoint)
+  local body, status_code, headers, status_description = https.request(endpoint)
 
-freeswitch.consoleLog("notice", string.format([[STATUS CODE: %d]], status_code))
-freeswitch.consoleLog("notice", string.format([[DATA: %s]], body))
+  freeswitch.consoleLog("notice", string.format([[STATUS CODE: %d]], status_code))
+  freeswitch.consoleLog("notice", string.format([[DATA: %s]], body))
+  if status_code == 200 then
+    return body
+  end
+end
+
+-- A simple method of making this module directly callable as a script, if it
+-- detects any arguments, then call the 'get_weather_data()' method with the first
+-- argument.
+if argv[1] then -- The first argument passed to the script.
+  _M.get_weather_data(argv[1])
+end
+
+-- Return the table of methods, this is what 'require' will return in the
+-- calling script.
+return _M
--- tips/02-parse-json.lua
+++ tips/02-parse-json.lua
@@ -2,19 +2,20 @@
   Lua doesn't come with a JSON parsing library, but FreeSWITCH includes one.
 ]]
 
+local inspect = require("inspect")
 local json = freeswitch.JSON()
+
+-- Encoding JSON.
 local data = {
   foo = "bar",
   baz = "bang",
 }
 local encoded_json = json:encode(data)
 freeswitch.consoleLog("notice", encoded_json)
-local json_string = [[
-{
-  "bing": "bang",
-  "bong": "blah"
-}
-]]
-local decoded_json = json:decode(json_string)
-freeswitch.consoleLog("notice", decoded_json.bing)
+
+-- Decoding JSON.
+local webservice = require("examples.04-webservice-call")
+local json_string = webservice.get_weather_data("15001")
+local data = json:decode(json_string)
+freeswitch.consoleLog("notice", inspect(data))
 
